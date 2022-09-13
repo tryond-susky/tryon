a06264dd3682abfe1ae3aa7dc7f01111ab1bd1be
tryond-susky <112492373+tryond-susky@users.noreply.github.com>
2022-09-13 10:28:36 -0400

    ahhhh

--- code/Projects/Week2_Plotting.Basics/Week2_Assignment.R
+++ code/Projects/Week2_Plotting.Basics/Week2_Assignment.R
@@ -8,27 +8,22 @@
 salmon <- c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o')
 sockeye <- c('w','w','w','w','w','y','y','y','y','y','n','n','n','n','n')
 king <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
-
 pink <- c(42,43,56,78,98,87,76,9,56,45,57,32,34,24,42)
-
 coho <- c(1.1,1.2,1.3,1.4,1.5,2.1,2.2,2.3,2.4,2.5,3.1,3.2,3.3,3.4,3.5)
-
 chum <- c(2,4,6,8,9,10,12,14,16,18,20,22,24,26,28)
 
-
 data <- cbind(salmon, chum, sockeye, king, pink, coho)
-
 data
 
 df <- as.data.frame(data)
 df
 
+
 colnames(df) <- c('Salmon', 'King','Pink','Chum','Coho','Sockeye')
 df
 
+
 row.names(df) <- df$Salmon
-
-
 df
 
 df[,-1]
@@ -43,9 +38,7 @@ df1
 colnames(df1) <- c('Salmon', 'King','Pink','Chum','Coho','Sockeye')
 df
 
-
 row.names(df1) <- df1$Salmon
-
 df1
 
 df1[,-1]
@@ -57,39 +50,36 @@ df1[,-1]
 # Export the plot as a PDF that is 4 inches wide and 7 inches tall.
 
 ?aggregate
+df$Coho <- as.numeric(as.character(df$Coho))
 
-df1$Coho <- as.numeric(1.1,1.2,1.3,1.4,1.5,2.1,2.2,2.3,2.4,2.5,3.1,3.2,3.3,3.4,3.5)
-df1$Sockeye
+df.mean <- aggregate(Coho ~Pink, data = df, FUN = "mean")
+df.mean
 
-df1.mean <- aggregate(FUN = "mean", df1$Coho ~df1$Sockeye)
-df1.mean
 
-colnames(df1.mean) <- c("Factor","Mean")
-df1.mean
+colnames(df.mean) <- c("Factor","Mean")
+df.mean
 
-barplot(df1.mean$Mean)
+barplot(df.mean$Mean)
 
-barplot(df.mean$Mean, 'Fish Scales' = df.mean$Factor)
-barplot(df1.mean$Mean, names.arg = df1.mean$Factor)
+barplot(df.mean$Mean, names.arg = df.mean$Factor)
 
-barplot(as.matrix(data))
+df.sd <- aggregate(df$Coho ~df$Pink, FUN = "sd")
 
-df1.sd <- aggregate(df1$Coho ~df1$Sockeye, FUN = "sd")
+colnames(df.sd) <- c("Factor","StanDev")
 
-colnames(df1.sd) <- c("Factor","StanDev")
-df1.sd
+df.sd
 
 b.plot <- barplot(df.mean$Mean, names.arg = df.mean$Factor)
-
-arrows(b.plot, df1.mean$Mean-df1.sd$StanDev,
-       b.plot, df1.mean$Mean+df1.sd$StanDev,angle=90,code=3)
-
-b.plot <- barplot(df.mean$Mean, names.arg = df.mean$Factor, ylim = c(0,5))
 
 arrows(b.plot, df.mean$Mean-df.sd$StanDev,
        b.plot, df.mean$Mean+df.sd$StanDev,angle=90,code=3)
 
-barplot(df.mean$Mean, names.arg = df.mean$Factor, ylim = c(0,5), horiz = TRUE)
+b.plot <- barplot(df.mean$Mean, names.arg = df.mean$Factor, ylim = c(0,100))
+
+arrows(b.plot, df.mean$Mean-df.sd$StanDev,
+       b.plot, df.mean$Mean+df.sd$StanDev,angle=90,code=3)
+
+barplot(df.mean$Mean, names.arg = df.mean$Factor, ylim = c(0,100), xlab = "Time", ylab = "Length", main = "Pacific Salmon 2022 Run", col = "dodgerblue2")
 
 ####Scatterplot
 # Create a scatter plot between two of your numeric columns.
@@ -106,6 +96,12 @@ demo('colors')
 
 # Upload both plots with the script used to create them to GitHub.
 # Follow the same file naming format as last week for the script.
+
+write.csv(df, file="week 2 assignment.csv") 
+getwd()
+
+write.csv(df, file = "Example.csv")#Extra code can be dangerous.
+
 write.csv(df1, file = "Example.csv")
 
 # Name plots as Lastname_barplot or Lastname_scatterplot. Save them to your "plots" folder.
