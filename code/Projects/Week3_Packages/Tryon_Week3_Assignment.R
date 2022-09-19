aa2641d18526981fd4af38e9d205323fec85c9c6
tryond-susky <112492373+tryond-susky@users.noreply.github.com>
2022-09-19 17:09:42 -0400

    g

--- code/Projects/Week3_Packages/Tryon_Week3_Assignment.R
+++ code/Projects/Week3_Packages/Tryon_Week3_Assignment.R
@@ -27,12 +27,10 @@ library(learnPopGen)
 
 drift.selection()
 p<-drift.selection(p0=0.01,Ne=100,w=c(1,0.9,0.8),ngen=200,nrep=5)
-print(p)
 plot(p)
 
 drift.selection()
 d<-drift.selection(p0=0.01,Ne=100,w=c(5,0.7,0.6),ngen=200,nrep=100, color=blues9)
-print(d)
 plot(d)
 
 
@@ -49,11 +47,69 @@ plot(d)
           # Diversity metrics are frequently used in community ecology for reasons ranging from a quick comparison between sites to understanding community stability.
           # Their calculation can be very tedious by hand - and very fast with a package designed for the operation.
 
-install.packages("diverse")
-library(diverse)
-balance(data=geese, category_row = TRUE)
-?balance
 
-balance(data=geese,category_row = FALSE)
+install.packages("simboot")
+library(simboot)
+rep.num <- c(1,2,3,1,2,3,1,2,3,5,2,3,4,0,5)
+uniqu.num <- c(0,1,1,2,3,5,8,13,21,34,55,89,144,233,377)
 
 
+
+sbdiv(rep.num, uniqu.num, theta = c("Shannon", "Simpson"),
+      type = c("Dunnett", "Tukey", "Sequen", "AVE",
+               "Changepoint", "Williams", "Marcus",
+               "McDermott", "UmbrellaWilliams", "GrandMean"),
+      cmat = NULL, method = c("WYht", "tsht", "rpht", "asht"), conf.level =
+        0.95, alternative = c("two.sided", "less", "greater"), R = 2000, base =
+        1)
+?sbdiv
+## For plots of the datasets see the help files for the data sets.
+
+## First dataset
+data(predatGM)
+
+## structure of data
+str(predatGM)
+
+## remove block variable
+datspec_1 <- predatGM[, -1]
+str(datspec_1)
+
+## Order of factorial variable
+datspec_1$Variety
+
+## argument base = 1 uses GM as control group. Not directly executable
+## due to intensive computing time
+# sbdiv(X = datspec_1[, 2:length(datspec_1)], f = datspec_1[, 1], theta =
+# "Shannon", type = "Dunnett", method = "WYht", conf.level = 0.95,
+# alternative = "two.sided", R = 2000, base = 1)
+
+## Directly executable but senseless value for boot steps R
+sbdiv(X = datspec_1[, 2:length(datspec_1)], f = datspec_1[, 1], theta =
+        "Shannon", type = "Dunnett", method = "WYht", conf.level = 0.95,
+      alternative = "two.sided", R = 100, base = 1)
+
+
+## Second dataset
+data(saproDipGM)
+
+## structure
+str(saproDipGM)
+
+## remove block variable
+datspec_2 <- saproDipGM[, -1]
+str(datspec_2)
+
+## Order of factor variable
+datspec_2$Variety
+
+## argument base = 2 uses Ins as control group. Not directly executable
+## due to intensive computing time
+# sbdiv(X = datspec_2[, 2:length(datspec_2)], f = datspec_2[, 1], theta =
+# "Shannon", type = "Dunnett", method = "rpht", conf.level = 0.95,
+# alternative = "two.sided", R = 2000, base = 2)
+
+## Directly executable but senseless value for boot steps R
+sbdiv(X = datspec_2[, 2:length(datspec_2)], f = datspec_2[, 1], theta =
+        "Shannon", type = "Dunnett", method = "rpht", conf.level = 0.95,
+      alternative = "two.sided", R = 100, base = 2)
