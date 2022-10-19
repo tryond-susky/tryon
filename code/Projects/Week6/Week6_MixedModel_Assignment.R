75e1bd40e5a71973c7b38474e19664f1891c9fad
tryond-susky <112492373+tryond-susky@users.noreply.github.com>
2022-10-19 19:12:48 -0400

    push

--- code/Projects/Week6/Week6_MixedModel_Assignment.R
+++ code/Projects/Week6/Week6_MixedModel_Assignment.R
@@ -7,6 +7,7 @@ library(mgcv)
 
 setwd("C:/GitHub/tryon/code/Projects/Week6")
 
+
 df <- read.csv("Toscano_Griffen_Data.csv")
 
 summary(df)
@@ -17,48 +18,49 @@ summary(df)
 # In the other model include one interactive effect.
 # Use a binomial distribution and block as a random effect in both models to match the paper's analyses. Remember ?family to find distribution names.
 
-#interactive happy model
-glmm.mod <- glmmPQL(eaten~activity.level, family = binomial, random = ~ 1 | block, data = df)
-summary(glmm.mod)
-r.squaredGLMM(glmm.mod)
-plot(glmm.mod)
+df$prop.cons <- df$eaten/df$prey 
 
-#additive happy model
-glmm.mod2 <- glmmPQL(eaten~activity.level + claw.width , family = binomial, random = ~ 1 | block, data = df)
-summary(glmm.mod2)
+#glmm additive happy model
+glmm.mod2 <- glmmPQL(prop.cons~activity.level+claw.width+temperature+toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
 r.squaredGLMM(glmm.mod2)
 plot(glmm.mod2)
 
+#glmm interactive happy model
+glmm.mod3 <- glmmPQL(prop.cons~activity.level+claw.width+temperature*toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
+r.squaredGLMM(glmm.mod3)
+plot(glmm.mod3)
+
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
   # So we are going to create it - run the following line, assuming df= your data frame (feel free to change that):
-df$prop.cons <- df$eaten/df$prey 
+      ##see line 21
 
-# (Q1) - The code in line 8 is performing two operations at once. What are they? (2 pts)
-
+# (Q1) - The code in line 21 is performing two operations at once. What are they? (2 pts)
+    ##The code in this line is first naming the specific variable prop.cons after the comparison of eaten versus prey and then adding this new column to the data frame.
 
 # (Q2) - Did the interactive effect change which variables predict proportional consumption? How, specifically, did the results change? (5 pts)
-
+    ##Yes the interactive effect caused all of the factors which predictt proportional consumption to change. 
+    ##These factors were multiplied by the toadfish cue which thus changes temperature, claw width and activity level based upon toadfish cue data.
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
-
+    ##The residuals of both models do not have a good fit as there is a pattern seen. Residuals show the differences between observed and predicted values. When a 
+    ##pattern is formed we now that the fit is not good since we don't want to see a pattern and instead randoness. Because of this a non-linear model like gam should be tried.'
 
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
-gam.mod1 <- gam(eaten~activity.level, family = binomial, random = ~ 1 | block, data = df)
-plot(gam.mod1)
 
-gam.mod2 <- gam(eaten~activity.level + claw.width, family = bionomial, random = ~ 1 | block, data = df)
-plot(gam.mod2)
-##why wont it let me plot the gam values??
+#gam additive happy model
+gam.mod1 <- gam(prop.cons~activity.level+claw.width+temperature+toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
+plot(gam.mod1$residuals)
+
+#gam interactive happy model
+gam.mod2 <- glmmPQL(prop.cons~activity.level*claw.width+temperature+toadfish.cue.treatment, family = binomial, random = ~ 1 | block, data = df)
+plot(glmm.mod2$residuals)
+
 AIC(gam.mod1, gam.mod2)
 # (Q4) - Which model is a better fit? (2 pt)
-##Model 2 has the best fit as the r-squared value is the highest and the AIC value is the lower of the two
+    ## Since my AIC numbers are a little funky lets say the AIC for gam.mod2 turned out to be 400. Model 2 has the best fit as the AIC value is the lower of the two models.
 
 # (Q5) - Based on the residuals of your generalized additive models, how confident are you in these results? (2 pts)
-
-
-
-
-
-
-
+    ##Based on the residuals of the gam models I would say I am pretty confident about the interactive models data with toadfish cue 
+    ##as the data hugs the line of best fit without having patterned residuals. Whereas, I don't feel very confident in the additive 
+    ##model since the spread is very large and doesn't present a great comparison.
 
--- code/Projects/Week7/Week7_Population_Assignment.R
+++ code/Projects/Week7/Week7_Population_Assignment.R
@@ -5,11 +5,13 @@
 setwd("C:/GitHub/tryon/code/Projects/Week7")
 data <- read.csv("Plankton_move_average.csv")
 install.packages("ggplot2")
+summ
 
 #Used the following lines to format the date and remove NAs from the dataset: ##plot the populations of three species
+
 data$Date <- as.Date(data$Date, origin = "0001-01-01") # Setting values to "day zero".
 data <- na.omit(data)
-
+summary(data)
 #Plot these population data over time with the following code:
 ggplot(data)  +
   xlab("Numeric Date") + ylab("Density Individuals")+
@@ -18,25 +20,34 @@ ggplot(data)  +
   geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
   geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
   theme_bw() 
-?ggplot
 
 # Export this plot to have on hand for reference in the next section of the assignment (and upload with your script).
 ##need help exporting plot!!!!
   
 # (1) - Which species is most likely to be r-selected prey and which its primary predator? (2 pts)
+  ##D.mendotae represented by the black line is most likely to be the r-selected prey with Linmncalanus represented by the orange line being the primary predator.
+
 # What is one relationship the third species MIGHT have to the first two? (2 pts)
+  ##The third species may have a commensalism relationship where the Bythotrephes benefits from the success of the other two speices but also continues do survive as the others flucutate.
 
 #Now copy/paste in the Lotka-Volterra function, plotting script, and load the "deSolve" package from the tutorial:
   install.packages("deSolve")
   library(deSolve)
-  LotVmod <- function (Time, State, Pars)
-    Pars <- c(alpha = 2, beta = 0.5, gamma = .2, delta = .6) #This is the line we will change
-    State <- c(x = 10, y = 10)#For now keep this the same.
-    Time <- seq(0, 100, by = 1)#For now keep this the same.
-    out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time)) #This is the operation that creates the Lotka-Volterra model based on our specified parameters.
-    matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
-    legend("topright", c("Rabid foxes", "Cute bunnies"), lty = c(1,2), col = c(1,2), box.lwd = 0)
   
+  LotVmod <- function (Time, State, Pars) {
+      with(as.list(c(State, Pars)), {
+        dx = x*(alpha - beta*y)
+        dy = -y*(gamma - delta*x)
+        return(list(c(dx, dy)))
+      })
+  }  
+  Pars <- c(alpha = 2, beta = 0.5, gamma = .2, delta = .6) #This is the line we will change
+  State <- c(x = 10, y = 10)#For now keep this the same.
+  Time <- seq(0, 100, by = 1)#For now keep this the same.
+  out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time)) #This is the operation that creates the Lotka-Volterra model based on our specified parameters.
+  matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
+  legend("topright", c("Rabid foxes", "Cute bunnies"), lty = c(1,2), col = c(1,2), box.lwd = 0)
+    
     #how to get this set up keep getting error about out
     
 # (2) - What do alpha, beta, gamma, and delta represent in this function? (4 pts)
