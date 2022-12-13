687356a4251fc13ca27d428922f6b4e029166f55
tryond-susky <112492373+tryond-susky@users.noreply.github.com>
2022-12-13 12:43:33 -0500

    Update R Workbook.R

--- code/Projects/Final Project/R Workbook.R
+++ code/Projects/Final Project/R Workbook.R
@@ -1,22 +1,28 @@
+library(stringr)
+library(readr)
+
 #setting working directory and inputting data
 
   setwd("C:/Github/tryon/code/Projects/Final Project")
 
 #Fish Data
-  Nelson <- read.csv("NelsonFishCounts.csv", header=TRUE)
-  Ilnik <- read.csv("IlnikFishCounts.csv", header=TRUE)
-  Bear <- read.csv("BearFishCounts.csv", header=TRUE)
-  Sandy <- read.csv("SandyFishCounts.csv", header=TRUE)
+  Bear <- read_csv("BearFishCounts.csv", col_types = cols(count_date = col_date(format = "%m/%d/%Y")))  
+  Nelson <- read_csv("NelsonFishCounts.csv", col_types = cols(count_date = col_date(format = "%m/%d/%Y")))
+  Ilnik <- read_csv("IlnikFishCounts.csv", col_types = cols(count_date = col_date(format = "%m/%d/%Y")))
+  Sandy <- read_csv("SandyFishCounts.csv", col_types = cols(count_date = col_date(format = "%m/%d/%Y")))  
   
   #Merge Weather Data
   fishdata <- rbind(Nelson,Ilnik,Bear,Sandy)
       
       #rename columns
       names(fishdata)[2] <- 'date'
+      
+      plot(fishdata$fish_count ~ fishdata$date)
+
 
 #Weather Data
 
-  weather <- read.csv("weatherdata.csv", header = TRUE)
+  weather  <- read_csv("weatherdata.csv", col_types = cols(Time = col_date(format = "%m/%d/%Y")))
   head(weather)
   colnames(weather)
 
@@ -50,7 +56,7 @@
         weatherdata <- merge(characterweather,numericweather, by = "date")
 
 #Merge Weather and Fish!!
-        data <- merge(weatherdata, fishdata, by = "date")
+        data <- as.data.frame(merge(weatherdata, fishdata, by = "date"))
         colnames(data) 
         #rename columns
         names(data) <- c('date', 'condition',"wind","temperature","dew.point","humidity","wind get rid of","wind.speed","wind.gust","pressure", "year","fish.count","species", "speices.id","location","location.id")
@@ -65,8 +71,28 @@
         # Change the x and y labels and add a title
         # Export the plot as a JPEG by using the "Export" button in the plotting pane.
         
-        plot(data$date ~ data$fish.count, ylim = 10,000,xlab = "Time", ylab = "Count", main = "Pacific Salmon 2022 Run", pch = 8 , col = "dodgerblue2")
+   
+              
+        plot(data$date, data$fish.count, xlab = "Time", ylab = "Count", main = "Pacific Salmon 2022 Run", pch = 8 , col = "dodgerblue2")
+     
+chinook <-subset(data, data$species == "Chinook")        
+chum  <-subset(data, data$species == "Chum")         
         
+        
+        
+        
+        
+plot(chinook$date, chinook$fish.count, type = "l" ,xlab = "Time", ylab = "Count", main = "Pacific Salmon 2022 Run", pch = 8 , col = "dodgerblue2")     
+lines(chum$date, chum$fish.count, type = "l", col = "red")        
+points(weatherdata$date, 10*weatherdata$`Wind Speed`)        
+ 
+
+
+plot(data$fish.count~factor(data$condition))
+
+  
+        barplot?
+          ?complete.cases
         ?plot.ecdf
         ?pch
         demo('colors')
